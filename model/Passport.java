4a6a354f1c12a81a38247684b82c83afb987d09f
yashmeshram1 <yashmeshram239@gmail.com0>
2025-08-31 11:10:12 +0530

    Added new folder to Spring-Boot repo

---
+++ model/Passport.java
@@ -0,0 +1,71 @@
+package com.day20session1que2.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "passport")
+public class Passport {
+  // Write your code here
+
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String passportNumber;
+
+  @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER,optional = false)
+  @JoinColumn(name = "forsa", referencedColumnName = "id")
+  private Person person;
+
+  public Passport() {
+  }
+
+  public Passport(String passportNumber, Person person) {
+    this.passportNumber = passportNumber;
+    this.person = person;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getPassportNumber() {
+    return passportNumber;
+  }
+
+  public void setPassportNumber(String passportNumber) {
+    this.passportNumber = passportNumber;
+  }
+
+  public Person getP() {
+    return person;
+  }
+
+  public void setP(Person p) {
+    this.person = person;
+  }
+
+  @Override
+  public String toString() {
+    return "Passport [id=" + id + ", passportNumber=" + passportNumber + ", p=" + person + "]";
+  }
+
+  
+
+  
+
+
+
+}
---
+++ model/Person.java
@@ -0,0 +1,63 @@
+package com.day20session1que2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+   
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private  Long id;
+
+   private String name;
+
+   @OneToOne (mappedBy = "person",fetch = FetchType.LAZY)
+   private Passport passport;
+
+   public Person() {
+   }
+
+   public Person(String name) {
+    this.name = name;
+   }
+
+   public Long getId() {
+    return id;
+   }
+
+   public void setId(Long id) {
+    this.id = id;
+   }
+
+   public String getName() {
+    return name;
+   }
+
+   public void setName(String name) {
+    this.name = name;
+   }
+
+   public Passport getPassport() {
+    return passport;
+   }
+
+   public void setPassport(Passport passport) {
+    this.passport = passport;
+   }
+
+   @Override
+   public String toString() {
+    return "Person [id=" + id + ", name=" + name + ", passport=" + passport + "]";
+   }
+
+   
+
+}
